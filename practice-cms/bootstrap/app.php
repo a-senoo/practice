dd614bce21a360a6466ff804e3fbbb58fb9fe2a1
Senoo Atsushi <a.senoo@hybrid-technologies.co.jp>
2024-12-14 17:27:23 +0900

    setting dot.env

--- practice-cms/bootstrap/app.php
+++ practice-cms/bootstrap/app.php
@@ -1,5 +1,17 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
+$repository = \Dotenv\Repository\RepositoryBuilder::createWithNoAdapters()
+    ->addAdapter(\Dotenv\Repository\Adapter\EnvConstAdapter::class)
+    ->addWriter(\Dotenv\Repository\Adapter\PutenvAdapter::class)
+    ->immutable()
+    ->make();
+
+$dotenv = \Dotenv\Dotenv::create($repository, __DIR__);
+$dotenv->load();
+
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
